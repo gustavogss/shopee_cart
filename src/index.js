534e8ff011e284b36d5f593004af68ee037f64f2
gustavogss <gustavogss.dev@gmail.com>
2025-06-29 02:23:53 -0300

    feat: create function addItem

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import {createItem} from "./services/item.js"
+
 const cart = [];
+
+console.log("Bem-vindo ao seu carrinho de compras da Shopee");
+
+const item1 = await createItem("IPhone16", 5.000, 1)
+const item2 = await createItem("IPhone13", 3.000, 2)
+
+console.log(`O subtotal de itens deu: R$ ${item2.subtotal().toFixed(3)},00`);
+
---
+++ src/services/cart.js
@@ -0,0 +1,15 @@
+async function addItem(userCart, item) {
+  userCart.push(item)
+}
+
+async function deleteItem(userCart, name) {
+  
+}
+
+async function removeItem(userCart, index) {
+  
+}
+
+async function calculateTotal(userCart) {
+  
+}
