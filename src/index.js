e05d71651e1be72677271f54bfa932438dd13b22
gustavogss <gustavogss.dev@gmail.com>
2025-06-29 02:55:48 -0300

    feat: create function calculateTotal

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import {createItem} from "./services/item.js"
+import {addItem, calculateTotal, deleteItem, removeItem} from "./services/cart.js"
 
 const cart = [];
+const myListFavorite = [];
 
-console.log("Bem-vindo ao seu carrinho de compras da Shopee");
+console.log("Bem-vindo ao seu carrinho de compras da Shopee \n");
 
-const item1 = await createItem("IPhone16", 5.000, 1)
-const item2 = await createItem("IPhone13", 3.000, 2)
+const item1 = await createItem("IPhone16", 7000, 1)
+const item2 = await createItem("IPhone13", 3000, 2)
 
-console.log(`O subtotal de itens deu: R$ ${item2.subtotal().toFixed(3)},00`);
+await addItem(myListFavorite, item1)
+await addItem(cart, item2)
+await calculateTotal(cart)
+
+
+
 
--- src/services/cart.js
+++ src/services/cart.js
@@ -1,15 +1,23 @@
-async function addItem(userCart, item) {
+export async function addItem(userCart, item) {
   userCart.push(item)
 }
 
-async function deleteItem(userCart, name) {
+export async function deleteItem(userCart, name) {
+  
   
 }
 
-async function removeItem(userCart, index) {
+export async function removeItem(userCart, index) {
   
 }
 
-async function calculateTotal(userCart) {
-  
+export async function calculateTotal(userCart) {
+  const result = userCart.reduce((total, item) => total + item.subtotal(), 0);
+  const resultFormat = result.toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+    minimumFractionDigits: 2
+  });
+  console.log(`Seu total de compras é: ${resultFormat} \n`);
+  return result;
 }
